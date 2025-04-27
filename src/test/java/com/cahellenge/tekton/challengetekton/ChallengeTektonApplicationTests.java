055ccdbffe7600feaa72f190b9ff170d7ca073dd
RaulGuzman <rauledenilsong@gmail.com>
2025-04-27 14:57:30 -0600

    agregando version de java al action

--- src/test/java/com/cahellenge/tekton/challengetekton/ChallengeTektonApplicationTests.java
+++ src/test/java/com/cahellenge/tekton/challengetekton/ChallengeTektonApplicationTests.java
@@ -2,12 +2,12 @@ package com.cahellenge.tekton.challengetekton;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChallengeTektonApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
--- src/test/java/com/cahellenge/tekton/challengetekton/service/CalculadoraServiceTest.java
+++ src/test/java/com/cahellenge/tekton/challengetekton/service/CalculadoraServiceTest.java
@@ -14,8 +14,10 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @EnableCaching
 @Import(CacheConfig.class)
